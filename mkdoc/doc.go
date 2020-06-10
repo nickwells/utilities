774591e229ebe08954d4adc6c4b379c60a21c13b
Nick Wells <wells.nicholas@gmail.com>
2020-06-10 23:35:42 +0100

    Add the documentary comment for the mkdoc program

---
+++ mkdoc/doc.go
@@ -0,0 +1,6 @@
+/*
+mkdoc will generate markdown files for any command that is using the
+param.mod module. It will rebuild the program and then call it with various
+help params to generate the markdown files.
+*/
+package main
