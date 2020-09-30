c5dbacbad9a1c05c71947f95ca71f2f998eb7b3f
Nick Wells <wells.nicholas@gmail.com>
2020-09-30 10:47:33 +0100

    Improve the documentation
    It removes a reference to fileChecker (the old name for this command) and
    expands the description of when 'gosh' generates '.orig' files

--- findCmpRm/doc.go
+++ findCmpRm/doc.go
@@ -1,6 +1,6 @@
 /*
 
-The fileChecker command finds all files in a given directory with a given
+The findCmpRm command finds all files in a given directory with a given
 extension and compares them against corresponding files without the
 extension. Then the user is prompted to delete the file with the extension.
 
@@ -8,6 +8,9 @@ It is most useful in conjunction with the testhelper package. The testhelper
 package will retain the original contents of a golden file in a file of the
 same name with an extension of '.orig'. This command will help you to review
 the changes and tidy up afterwards.
+
+The gosh command also generates files with an extension of '.orig' when
+editing files in place and these can be cleared up using this command.
 
 */
 package main
