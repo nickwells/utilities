ba2aefa7847257df24daa5bfb1c63e48c2bdf3b0
Nick Wells <wells.nicholas@gmail.com>
2021-06-09 17:36:44 +0100

    Remove the commented-out line

--- gosh/gosh.go
+++ gosh/gosh.go
@@ -136,7 +136,6 @@ func snippetExpand(g *Gosh, sName string) ([]string, error) {
 	}
 
 	var content []string
-	//addSnippetComment(&content, s.Path())
 	addSnippetComment(&content, "BEGIN")
 	content = append(content, "// "+s.Path())
 	content = append(content, s.Text()...)
