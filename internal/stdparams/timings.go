52404572468a6c02329ae65b9b51050cc91381a4
Nick Wells <wells.nicholas@gmail.com>
2021-06-29 21:57:18 +0100

    Add the internal packages callstack and stdparams
    callstack is used to report timings and to give tags to debug (verbose)
    messages that reflect the call stack
    
    stdparams is used to hold common parameters to ensure that they use the same
    names across programs. Currently only the AddTiming func, used to add the
    show-timings parameter exists.
    
    These packages may "graduate" in the future out of the internal playpen but
    for now it allows some experimentation without needing to support the
    packages and freeze the API.

---
+++ internal/stdparams/timings.go
@@ -0,0 +1,23 @@
+package stdparams
+
+import (
+	"github.com/nickwells/param.mod/v5/param"
+	"github.com/nickwells/param.mod/v5/param/psetter"
+	"github.com/nickwells/utilities/internal/callstack"
+)
+
+// AddTiming adds the common show-timing parameter used to set the
+// ShowTimings field in a callstack.Stack struct
+func AddTiming(
+	ps *param.PSet,
+	cs *callstack.Stack,
+	opt ...param.OptFunc,
+) *param.ByName {
+	opt = append(opt,
+		param.Attrs(param.DontShowInStdUsage|param.CommandLineOnly),
+		param.AltNames("show-timing", "show-time", "show-times"))
+
+	return ps.Add("show-timings", psetter.Bool{Value: &cs.ShowTimings},
+		"report the time taken for various parts of this program to complete.",
+		opt...)
+}
