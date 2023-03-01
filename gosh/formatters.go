aa02409bfe9542f4231dd8980d7f523597da04fe
Nick Wells <wells.nicholas@gmail.com>
2023-03-01 16:44:40 +0000

    refactor(gosh): add the findFormatter func

--- gosh/formatters.go
+++ gosh/formatters.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"os/exec"
 	"strings"
 
 	"github.com/nickwells/english.mod/english"
+	"github.com/nickwells/verbose.mod/verbose"
 )
 
 // Formatter holds the name of a program that can "format" the generated
@@ -14,12 +16,17 @@ type Formatter struct {
 	args []string
 }
 
+// formatters is a list of default commands that can be used to 'format' the
+// code. Note that we are only interested in the side effect, namely that of
+// populating the import statement. These two independed functions should be
+// split into two separate tasks.
 var formatters = []Formatter{
 	{name: "gopls", args: []string{"imports", "-w"}},
 	{name: "goimports", args: []string{"-w"}},
 	{name: "gofmt", args: []string{"-w"}},
 }
 
+// formatterCmds will return a string describing the available commands
 func formatterCmds() string {
 	fCmds := make([]string, 0, len(formatters))
 
@@ -29,4 +36,23 @@ func formatterCmds() string {
 	}
 
 	return english.Join(fCmds, ", ", " or ")
+}
+
+// findFormatter will find the first available formatter in the PATH. it
+// returns the formatter, the full path and true if the formatter is found,
+// empty values and false otherwise.
+func findFormatter(g *Gosh) (Formatter, string, bool) {
+	defer g.dbgStack.Start("findFormatter", "Finding the formatting command")()
+	intro := g.dbgStack.Tag()
+
+	for _, f := range formatters {
+		if path, err := exec.LookPath(f.name); err == nil {
+			verbose.Println(intro, " Using the default formatter: ", f.name)
+			verbose.Println(intro, "                    pathname: ", path)
+			verbose.Println(intro, "                   arguments: ",
+				strings.Join(f.args, " "))
+			return f, path, true
+		}
+	}
+	return Formatter{}, "", false
 }
